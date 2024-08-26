bd6b4158125069cf238e28824527849173796635
Naman Malhotra <71208368+naman-mh@users.noreply.github.com>
2024-08-26 11:18:47 +0000

    new

--- restaurent/src/App.js
+++ restaurent/src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
-import NavbarStatus from './component /Layout/NavbarStatus';
+
 
 
 function App() {
   return (
-    <div className="App">
-      <NavbarStatus/>
-    </div>
+   <>
+    <h1>Again </h1>
+   </>
   );
 }
 
--- restaurent/src/component /Layout/NavbarStatus.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NavbarStatus = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
-
-export default NavbarStatus;
