b9ae0d41a6238b457a4f8767cb339579d85efbea
Naman Malhotra <71208368+naman-mh@users.noreply.github.com>
2024-08-27 13:58:23 +0000

    New Work Add

--- restaurent/src/App.js
+++ restaurent/src/App.js
@@ -1,13 +1,24 @@
-
-import './App.css';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavComponent from "./components/NavComponent";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 
 function App() {
   return (
-   <>
-    <h1>Again </h1>
-   </>
+    <>
+      <BrowserRouter>
+        <NavComponent/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/login" element={<Login/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
