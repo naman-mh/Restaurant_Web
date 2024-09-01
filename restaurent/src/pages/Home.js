747a79f608095e96e4b88a9cecfa61c462c70563
Naman Malhotra <namanmalhotra9759@gmail.com>
2024-09-01 21:19:51 +0530

    some changes done in home component

--- restaurent/src/pages/Home.js
+++ restaurent/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useRef } from "react";
 import { Box, Typography } from "@mui/material";
 import img1 from "../Assets/image1.jpeg";
 import img2 from "../Assets/image2.jpeg";
@@ -6,34 +7,45 @@ import img3 from "../Assets/optical-chemist-uz7dXgkdvkU-unsplash.jpg";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css"
 import "swiper/css/pagination"
-import { Pagination } from "swiper/modules";
+import { Pagination ,Autoplay ,  } from "swiper/modules";
 
 function Home() {
+  const progressCircle = useRef(null);
+  const progressContent = useRef(null);
+  const onAutoplayTimeLeft = (s, time, progress) => {
+    progressCircle.current.style.setProperty('--progress', 1 - progress);
+    progressContent.current.textContent = `${Math.ceil(time / 1000)}s`; }
   return (
-    <Swiper
-      pagination={true} modules={[Pagination]} >
-      <SwiperSlide>
-        <Box sx={{
-          height: "70vh",
-          backgroundImage: `url(${img1})`,
-          backgroundSize: "cover",
+    <>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
         }}
-        ></Box>
-        <Typography>slide1</Typography>
-      </SwiperSlide>
-      <SwiperSlide>
-        <Box sx={{
-          height: "70vh",
-          backgroundImage: `url(${img2})`,
-          backgroundSize: "cover",
-        }}>
-
-        </Box>
-          <Typography>slide2</Typography>
-      </SwiperSlide>
-    </Swiper>
-
-  );
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination ]}
+        onAutoplayTimeLeft={onAutoplayTimeLeft}
+        className="mySwiper"
+      >
+        <div className="autoplay-progress" slot="container-end">
+          <svg viewBox="0 0 48 48" ref={progressCircle}>
+            <circle cx="24" cy="24" r="20"></circle>
+          </svg>
+          <span ref={progressContent}></span>
+        </div>
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+      </Swiper>
+    </>
+  )
 }
 
 export default Home;
