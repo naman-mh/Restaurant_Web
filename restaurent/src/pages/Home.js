afddbee59671da21531027f1b4ed60327ee7d0b6
Naman Malhotra <71208368+naman-mh@users.noreply.github.com>
2024-09-02 01:17:27 +0000

    some update

--- restaurent/src/pages/Home.js
+++ restaurent/src/pages/Home.js
@@ -4,17 +4,19 @@ import { Box, Typography } from "@mui/material";
 import img1 from "../Assets/image1.jpeg";
 import img2 from "../Assets/image2.jpeg";
 import img3 from "../Assets/optical-chemist-uz7dXgkdvkU-unsplash.jpg";
+import img4 from "../Assets/image3.jpeg"
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css"
 import "swiper/css/pagination"
-import { Pagination ,Autoplay ,  } from "swiper/modules";
+import { Pagination, Autoplay, } from "swiper/modules";
 
 function Home() {
   const progressCircle = useRef(null);
   const progressContent = useRef(null);
   const onAutoplayTimeLeft = (s, time, progress) => {
     progressCircle.current.style.setProperty('--progress', 1 - progress);
-    progressContent.current.textContent = `${Math.ceil(time / 1000)}s`; }
+    // progressContent.current.textContent = `${Math.ceil(time / 1000)}s`; }
+  }
   return (
     <>
       <Swiper
@@ -28,21 +30,52 @@ function Home() {
           clickable: true,
         }}
         navigation={true}
-        modules={[Autoplay, Pagination ]}
+        modules={[Autoplay, Pagination]}
         onAutoplayTimeLeft={onAutoplayTimeLeft}
         className="mySwiper"
       >
         <div className="autoplay-progress" slot="container-end">
-          <svg viewBox="0 0 48 48" ref={progressCircle}>
+          <svg viewBox="0 0 0 0" ref={progressCircle}>
             <circle cx="24" cy="24" r="20"></circle>
           </svg>
           <span ref={progressContent}></span>
         </div>
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>
+          <Box sx={{
+            backgroundImage:"url{img1}"
+          }}>
+            
+          </Box>
+        </SwiperSlide>
+        <SwiperSlide><Box sx={{
+          height: "60vh",
+          width: "100vw"
+
+        }}>
+          <img src={img2} />
+        </Box></SwiperSlide>
+        <SwiperSlide><Box sx={{
+          height: "60vh",
+          width: "100vw"
+
+        }}>
+          <img src={img3} />
+        </Box></SwiperSlide>
+        <SwiperSlide>
+          <Box sx={{
+            height: "60vh",
+            width: "100vw"
+
+          }}>
+            <img src={img3} />
+          </Box></SwiperSlide>
+        <SwiperSlide><Box sx={{
+          height: "60vh",
+          width: "100vw"
+
+        }}>
+          <img src={img4} />
+        </Box></SwiperSlide>
       </Swiper>
     </>
   )
